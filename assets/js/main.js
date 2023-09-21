7ab0dd228688bfbb73cb84429b0a3929162685f7
AS <alainsaas@gmail.com>
2023-09-20 23:10:43 -0400

    Faster animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,7 +26,7 @@ TxtType.prototype.tick = function() {
   } else if (this.isDeleting && this.txt === '') {
     this.isDeleting = false;
     this.loopNum++;
-    delta = 500;
+    delta = 400;
   }
   setTimeout(function() {
     that.tick();
